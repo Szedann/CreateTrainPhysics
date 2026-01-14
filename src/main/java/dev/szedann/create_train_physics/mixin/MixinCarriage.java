59b05fcd4c51c04dc6cfefd2be8d3c1872beb263
Szedann <65607498+Szedann@users.noreply.github.com>
2026-01-14 16:09:04 +0100

    save data

--- src/main/java/dev/szedann/create_train_physics/mixin/MixinCarriage.java
+++ src/main/java/dev/szedann/create_train_physics/mixin/MixinCarriage.java
@@ -2,11 +2,18 @@ package dev.szedann.create_train_physics.mixin;
 
 import com.simibubi.create.content.trains.entity.Carriage;
 import com.simibubi.create.content.trains.entity.CarriageContraptionEntity;
+import com.simibubi.create.content.trains.graph.DimensionPalette;
+import com.simibubi.create.content.trains.graph.TrackGraph;
 import dev.szedann.create_train_physics.accessors.IPhysicsCarriage;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -23,6 +30,27 @@ public abstract class MixinCarriage implements IPhysicsCarriage {
     @Unique
 
     private @Nullable Integer trainphys$engineCount = null;
+
+    @Inject(method = "write", at = @At("RETURN"))
+    private void writeMassAndEngineCount(DimensionPalette dimensions, HolderLookup.Provider registries, CallbackInfoReturnable<CompoundTag> cir){
+        CompoundTag tag = cir.getReturnValue();
+        Integer mass = railways$getMass();
+        if(mass != null)
+            tag.putInt("mass", mass);
+        Integer engineCount = trainphys$getEngineCount();
+        if(engineCount != null)
+            tag.putInt("engineCount", engineCount);
+    }
+
+    @Inject(method = "read", at = @At("RETURN"))
+    private static void readMassAndEngineCount(CompoundTag tag, HolderLookup.Provider registries, TrackGraph graph, DimensionPalette dimensions, CallbackInfoReturnable<Carriage> cir) {
+        IPhysicsCarriage carriage = (IPhysicsCarriage) cir.getReturnValue();
+
+        if(tag.contains("mass", CompoundTag.TAG_INT))
+            carriage.railways$setMass(tag.getInt("mass"));
+        if(tag.contains("engineCount", CompoundTag.TAG_INT))
+            carriage.trainphys$setEngineCount(tag.getInt("engineCount"));
+    }
 
     @Override
     public @Nullable Integer railways$getMass(){
@@ -49,7 +77,7 @@ public abstract class MixinCarriage implements IPhysicsCarriage {
                         engineCount.getAndIncrement();
                     }
                 });
-                trainphys$engineCount = engineCount.get();
+                trainphys$setEngineCount(engineCount.get());
             }
         }
         return trainphys$engineCount;
@@ -57,6 +85,6 @@ public abstract class MixinCarriage implements IPhysicsCarriage {
 
     @Override
     public void trainphys$setEngineCount(int engineCount) {
-
+        trainphys$engineCount = engineCount;
     }
 }
